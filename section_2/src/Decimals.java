75f3ec2e8e2636964ef28f8cad0b6d21f9c4c083
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-15 10:39:05 +0000

    double

---
+++ section_2/src/Decimals.java
@@ -0,0 +1,17 @@
+public class Decimals {
+
+    public static void main( String[] args){
+        double percentage = 55.7;
+        System.out.println("In 2020, " + percentage + "% of the world's pop is urban");
+
+//        double in maths calculation
+        double dividend = 25;
+        double divisor = 2;
+        System.out.println(dividend/divisor);
+
+//        Use int to store whole numbers
+//        Use long to store very large whole numbers
+//        Use double to store and work with decimals
+
+    }
+}
