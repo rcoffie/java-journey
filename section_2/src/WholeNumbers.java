d732b25ea51f91bb6091b44611ba042cc7dca66a
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-15 09:41:20 +0000

    long and int types

---
+++ section_2/src/WholeNumbers.java
@@ -0,0 +1,14 @@
+public class WholeNumbers {
+    public static void main(String[] args){
+
+//        int type
+        int numberOfFans = 5000;
+        System.out.println("There is " + numberOfFans + " fans at the Quidditch games");
+
+//        long type
+//        Note kinly add L at the end of every number to let Java knows is a large number
+        long globalPopulation = 7000000000L;
+        System.out.println("There are " + globalPopulation + " people on earth");
+
+    }
+}
