6540fe0de8c6fc0d1dd592c4920fcddcedd944eb
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-16 13:53:01 +0000

    type_casting

---
+++ section_2/src/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+    public static void main(String[] args){
+        double salary = 2100.5456;
+        System.out.println("Percy's monthly salary is $" +(salary));
+        int roundedSalary = (int)salary;
+        System.out.println("Percy's monthly salary rounded $" + ( roundedSalary));
+
+    }
+}
