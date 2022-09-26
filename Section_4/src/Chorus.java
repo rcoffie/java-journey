8d01a4d5c161af33e1c3f64f87b571f041f06461
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-26 20:50:12 +0000

    function

---
+++ Section_4/src/Chorus.java
@@ -0,0 +1,11 @@
+public class Chorus {
+    public static void main(String[] args){
+          singChrous();
+    }
+    public static void singChrous(){
+        System.out.println("Don't blame it on the sunshine ");
+        System.out.println("Don't blame in on the the moonlight ");
+        System.out.println("Don't blame it on good times ");
+        System.out.println("Blame it on the boogie\n");
+    }
+}
