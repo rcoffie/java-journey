935232b6ce0d690a0e798caa642298b568304192
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-29 10:25:06 +0000

    function

---
+++ Section_4/src/JavaBuildInFuctions.java
@@ -0,0 +1,21 @@
+public class JavaBuildInFuctions{
+    public static void main(String [] args){
+//        println function this perfom the task of printing you message onto the console
+        System.out.println("hello Java ");
+//        using Java Build In Functions+
+//        the math.log function
+        double logg = Math.log(10.4);
+        System.out.println("The log of 10.4 is : " + logg);
+
+        double sine = Math.sin(1.2);
+        System.out.println("The Sin of 1.2 is :" + sine);
+
+        double power = Math.pow(3,5);
+
+        System.out.println(" 3 to the power of 5 equals " + power);
+
+        double random = Math.random();
+        System.out.println(" this is your random number " + random );
+
+    }
+}
