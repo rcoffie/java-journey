59f76b70100c55bcd3e58c45e1a3e9b8b85da693
Robert Coffie <rcoffie22@yhahoo.com>
2022-10-03 14:02:54 +0000

    looping through and array

---
+++ section_6/src/ArraysAndLoops.java
@@ -0,0 +1,10 @@
+public class ArraysAndLoops {
+    public static void main(String[] args){
+    String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "E A"};
+        System.out.println("The number of element is " + kingdoms.length );
+        for (int i = 0; i < kingdoms.length; i++){
+            System.out.println("The element at of element  "+ i + " is " + kingdoms[i]);
+        }
+
+    }
+}
